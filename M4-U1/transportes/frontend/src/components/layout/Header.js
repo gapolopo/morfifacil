ea5cd13ddffe91c3da56ed95d0024a11e0cd98e4
Galo Budiño <GBUDINO@claro.com.ar>
2022-09-20 09:12:00 -0300

    Se sube tarea M4-U1. Saludos

---
+++ M4-U1/transportes/frontend/src/components/layout/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <header>
+      <div className="holder">
+        <img src="images/morfifacil.png" width="100" alt="MorfiFacil" />
+        <h1>Morfi-Facil</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ M4-U1/transportes/frontend/src/pages/HomePage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const HomePage = (props) => {
+  return (
+    <main className="holder">
+      <div className="homeimg">
+        <img src="images/home.png" alt="Morfi-Facil"/>
+      </div>
+      <div className="columnas">
+        <div className="bienvenidos">
+          <h2>Bienvenidos</h2>
+          Esta aplicación dará solución a la problemática diaria de las comidas (desayuno, almuerzo, merienda, cena y entre comidas), intentando poner un orden nutricional a esta problemática.
+          
+          Es real el dilema de toda familia, no saber que comer y, si para cierta preparación, tenemos los ingredientes en la alacena.
+
+          Esta solución contará con un calendario de comidas, una biblioteca de recetas y una lista de almacén que propondrá, basado en las recetas disponibles y el consumo de ingredientes, que productos hay que reponer.
+
+          Lo invitamos a utilizar esta solución para brindarle a su familia una dieta más equilibrada a la vez que soluciona problemas típicos y frecuentes, y ayuda a la contabilidad familiar.
+
+          Saludos
+        </div>
+        <div className="testimonios">
+          <h2>Testimonios</h2>
+          <div className="testimonio">
+            <span class="cita"> Simplemente Excelente</span>
+            <span className="autor"> Juan Perez - zapatos.com </span>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default HomePage;
